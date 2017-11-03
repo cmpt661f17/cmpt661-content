56004c42d6a129cf1de0dbef2d06f57768e8642b
ae <erradi@live.com>
2017-11-03 19:58:25 +0300

    Updated MVC Examples

--- Examples/07.MVC-JavaScript-WebApp/controllers/HeroController.js
+++ Examples/07.MVC-JavaScript-WebApp/controllers/HeroController.js
@@ -2,98 +2,98 @@ const heroRepository = require('./../models/HeroRepository');
 
 class HeroController {
     async getHeroes(req, res) {
-        const heroes = await heroRepository.getHeroes()
-        res.json(heroes)
+        const heroes = await heroRepository.getHeroes();
+        res.json(heroes);
     }
 
     async getHero (req, res) {
         try {
-            const heroId = req.params.id
-            console.log('getHero.req.params.id', heroId)
+            const heroId = req.params.id;
+            console.log('getHero.req.params.id', heroId);
             const hero = await heroRepository.getHero(parseInt(heroId))
-            console.log(JSON.stringify(hero, null, 2))
-            res.json(hero)
+            console.log(JSON.stringify(hero, null, 2));
+            res.json(hero);
         }
         catch (err) {
-            res.status(404).send(err)
+            res.status(404).send(err);
         }
     }
 
     async addHero(req, res) {
         try {
             let hero = req.body
-            hero = await heroRepository.addHero(hero)
-            const urlOfNewHero = `/api/heroes/${hero.id}`
-            res.location(urlOfNewHero)
-            res.status(201).send("created")
+            hero = await heroRepository.addHero(hero);
+            const urlOfNewHero = `/api/heroes/${hero.id}`;
+            res.location(urlOfNewHero);
+            res.status(201).send("created");
         }
         catch (err) {
-            console.log(err)
-            res.status(500).send(err)
+            console.log(err);
+            res.status(500).send(err);
         }
     }
 
     async updateHero(req, res) {
         try {
-            const hero = req.body
+            const hero = req.body;
 
-            await heroRepository.updateHero(hero)
-            res.status(200).send("ok")
+            await heroRepository.updateHero(hero);
+            res.status(200).send("ok");
         }
         catch (err) {
-            res.status(500).send(err)
+            res.status(500).send(err);
         }
     }
 
     async deleteHero(req, res) {
         try {
-            const heroId = req.params.id
+            const heroId = req.params.id;
 
-            await heroRepository.deleteHero(heroId)
-            res.status(200).send("ok")
+            await heroRepository.deleteHero(heroId);
+            res.status(200).send("ok");
         }
         catch (err) {
-            res.status(500).send(err)
+            res.status(500).send(err);
         }
     }
 
     async index (req, res) {
-        const heroes = await heroRepository.getHeroes()
+        const heroes = await heroRepository.getHeroes();
 
-        res.render('heroes', {heroes})
+        res.render('heroes', {heroes});
     }
 
     async heroForm (req, res) {
-        const heroId = req.params.id
+        const heroId = req.params.id;
         let hero;
 
-        console.log("heroForm.heroId", heroId)
+        console.log("heroForm.heroId", heroId);
 
         //If heroId is not equal to new and if it is a number then get the hero to pass it to the template
         if (heroId != 'new' && !isNaN(parseInt(heroId)) ) {
-            hero = await heroRepository.getHero( parseInt(heroId) )
+            hero = await heroRepository.getHero( parseInt(heroId) );
         }
 
-        res.render('hero-form', hero )
+        res.render('hero-form', hero );
     }
 
     async postHero(req, res) {
         try {
-            const hero = req.body
+            const hero = req.body;
 
             if (hero.id === '') {
-                await heroRepository.addHero(hero)
+                await heroRepository.addHero(hero);
             } else {
-                hero.id = parseInt(hero.id)
-                await heroRepository.updateHero(hero)
+                hero.id = parseInt(hero.id);
+                await heroRepository.updateHero(hero);
             }
-            res.redirect("/heroes")
+            res.redirect("/heroes");
         }
         catch (err) {
-            console.log(err)
-            res.status(500).send(err)
+            console.log(err);
+            res.status(500).send(err);
         }
     }
 }
 
-module.exports = new HeroController()
+module.exports = new HeroController();
--- Examples/07.MVC-JavaScript-WebApp/controllers/StudentController.js
+++ Examples/07.MVC-JavaScript-WebApp/controllers/StudentController.js
@@ -1,30 +1,30 @@
-const studentRepository = require('./../models/StudentRepository')
+const studentRepository = require('./../models/StudentRepository');
 
 class StudentController {
     async getStudents(req, res) {
-        let students = await studentRepository.getStudents()
-        res.json(students)
+        let students = await studentRepository.getStudents();
+        res.json(students);
     }
 
     async getStudent(req, res) {
         try {
-            let studentId = req.params.id
-            console.log('getStudent.req.params.id', studentId)
+            let studentId = req.params.id;
+            console.log('getStudent.req.params.id', studentId);
 
-            let student = await studentRepository.getStudentCourses(parseInt(studentId))
+            let student = await studentRepository.getStudentCourses(parseInt(studentId));
             //console.log(JSON.stringify(student, null, 2))
-            res.json(student)
+            res.json(student);
         }
         catch (err) {
-            console.log(err)
-            res.status(500).send(err)
+            console.log(err);
+            res.status(500).send(err);
         }
     }
 
     async index (req, res) {
-        let students = await studentRepository.getStudents()
-        res.render('student', { students })
+        let students = await studentRepository.getStudents();
+        res.render('student', { students });
     }
 }
 
-module.exports = new StudentController()
+module.exports = new StudentController();
